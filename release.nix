56b87a57a00fc26f4165b94359bbc71b9deec130
Ali Abrar <aliabrar@gmail.com>
2019-08-04 12:44:11 -0400

    Fix some version bounds; Remove GHC822 from CI; Build against stable and unstable reflex-platform

--- release.nix
+++ release.nix
@@ -1,2 +1,6 @@
-{ rp ? import ./reflex-platform {}
-}: rp.ghc.callCabal2nix "reflex-vty" ./. {}
+{ p ? import ./reflex-platform/stable {}
+}:
+let
+  inherit (p.nixpkgs) lib;
+  platforms = p.thunkSet ./reflex-platform;
+in (lib.mapAttrs (name: platform: (import platform {}).ghc.callCabal2nix "reflex-vty" ./. {})) platforms 
