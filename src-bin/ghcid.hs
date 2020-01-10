555d40c6b403c20a2ab740a73ffe20e734ce1796
Ali Abrar <aliabrar@gmail.com>
2020-01-09 23:00:53 -0500

    Add some comments and TODOs to the ghcid example

--- src-bin/ghcid.hs
+++ src-bin/ghcid.hs
@@ -18,6 +18,7 @@ import Data.Sequence (Seq)
 import Data.String (IsString)
 import qualified Data.Text.Encoding as T
 import qualified Graphics.Vty as V
+import Reflex.Network (networkHold)
 import Text.Regex.TDFA as Regex
 import Text.Regex.TDFA.ByteString ()
 import qualified System.Directory as Dir
@@ -26,6 +27,7 @@ import qualified System.FSNotify as FS
 import System.IO.Temp (withSystemTempDirectory)
 import qualified System.Process as P
 
+-- TODO: Move this to a common module
 watchDirectory
   :: (Reflex t, TriggerEvent t m, PerformEvent t m, MonadIO (Performable m))
   => FS.WatchConfig
@@ -50,14 +52,19 @@ ghcid
      , MonadHold t m
      )
   => FilePath
+  -- ^ Temporary directory where we can put a ".ghci" file
   -> m (Ghci t)
 ghcid tempDir = do
   dir <- liftIO Dir.getCurrentDirectory
+  -- Create a .ghci file with our custom settings
+  -- TODO: Should the .ghci file be customizable by users of this function?
   let dotGhci = tempDir FS.</> ".ghci"
   liftIO $ writeFile dotGhci $ unlines
     [ ":set prompt " <> prompt
     ]
   pb <- getPostBuild
+
+  -- Watch the project directory for changes
   fsEvents <- watchDirectory (noDebounce FS.defaultConfig) (dir <$ pb)
   -- TODO Handle changes to "src" and ".cabal" differently. ":r" is only really appropriate
   -- when there are changes to ghcid.hs (this file) given the cabal repl command we're using.
@@ -68,25 +75,61 @@ ghcid tempDir = do
         in case subpath of
              Nothing -> False
              Just s -> not $ List.isPrefixOf ["/", ".git/"] s || List.isPrefixOf ["/", "dist/"] s
+  -- Events are batched because otherwise we'd get several updates corresponding to one
+  -- user-level change. For example, saving a file in vim results in an event claiming
+  -- the file was removed followed almost immediately by an event adding the file
   batchedFsEvents <- batchOccurrences 0.1 filteredFsEvents
+
+  -- Construct the cabal repl command line
+  -- TODO: This should be configurable
   let cabalRepl = (P.proc "cabal"
         [ "repl"
         , "ghcid"
         , "--ghc-options=-Wall"
         , "--ghc-options=-ignore-dot-ghci"
         , "--ghc-options=-ghci-script " <> dotGhci
-        ]) { P.create_group = True }
+        ]) { P.create_group = True } -- It's important that we're in our own process group
+                                     -- so that we can interrupt without killing the parent
+                                     -- (vty) process
+
+  -- Request a reload every time the files we're watching change
+  let reload = ":r" <$ batchedFsEvents
+
+  -- Run the process and feed it some input:
   rec proc <- createProcess cabalRepl $ ProcessConfig
         { _processConfig_stdin = leftmost
             [ reload
+            -- Execute some expression if ghci is ready to receive it
+            -- TODO: This should be configurable
             , fmap (const "test") $ ffilter id $ updated testMode
             ]
         , _processConfig_signal = never
         }
+
+      -- Capture and accumulate stdout between reloads. We'll inspect this value
+      -- to determine ghci's state
+      output <- foldDyn ($) "" $ leftmost
+        [ flip mappend <$> _process_stdout proc
+        , const "Reloading...\n" <$ batchedFsEvents
+        ]
+
+      -- We need to know when ghci is initialized enough that it won't die when
+      -- it receives a sigint. We wait to see the version line in the output as
+      -- a proxy for ghci's readiness to be interrupted
+      initialized <- hold False $ fforMaybe (updated output) $ \o ->
+        if o Regex.=~ ("GHCi, version.*: http://www.haskell.org/ghc/" :: BS.ByteString)
+          then Just True
+          else Nothing
+
+     -- Only interrupt when there's a file change and we're ready and not in an idle state
       let interruptible ls ready = ready && ls == LoadState_Loading
           requestInterrupt = gate (interruptible <$> current loadState <*> initialized) batchedFsEvents
       performEvent_ $ ffor requestInterrupt $
         const $ liftIO $ P.interruptProcessGroupOf $ _process_handle proc
+
+      -- Keep track of whether we're in a position from which an expression can be evaluated
+      -- We enter this mode upon successfully loading the requested modules.
+      -- TODO: What if the expression fails?
       testMode <- holdDyn False $ leftmost
         [ False <$ requestInterrupt
         , False <$ reload
@@ -95,11 +138,8 @@ ghcid tempDir = do
             LoadState_Failed -> Just False
             _ -> Nothing
         ]
-      let reload = ":r" <$ batchedFsEvents
-      output <- foldDyn ($) "" $ leftmost
-        [ flip mappend <$> _process_stdout proc
-        , const "Reloading...\n" <$ batchedFsEvents
-        ]
+
+      -- Inspect the output and determine what state ghci is in
       let loadState = ffor output $ \o ->
             case reverse (C8.lines o) of
               (expectedPrompt:expectedMessage:_) ->
@@ -109,10 +149,10 @@ ghcid tempDir = do
                     else LoadState_Failed
                   else LoadState_Loading
               _ -> LoadState_Loading
-      initialized <- hold False $ fforMaybe (updated output) $ \o ->
-        if o Regex.=~ ("GHCi, version.*: http://www.haskell.org/ghc/" :: BS.ByteString)
-          then Just True
-          else Nothing
+
+  -- Below, we split up the output of the ghci process into things that ghci itself
+  -- produces (e.g., errors, warnings, loading messages) and the output of the expression
+  -- it is evaluating
   return $ Ghci
     { _ghci_moduleOut = gate (not <$> current testMode) $ _process_stdout proc
     , _ghci_moduleErr = gate (not <$> current testMode) $ _process_stderr proc
@@ -159,19 +199,27 @@ main = withSystemTempDirectory "reflex-ghcid" $ \tempDir -> mainWidget $ do
           , flip mappend <$> _ghci_moduleErr ghci
           , const "" <$ _ghci_filesystem ghci
           ]
-        stretch $ scrollableText never $ T.decodeUtf8 <$> current out
+        (dh, scroll) <- stretch $ do
+          dh <- displayHeight
+          scroll <- scrollableText never $ T.decodeUtf8 <$> current out
+          return (dh, scroll)
+        fixed 1 $ text $ (\h (ix, n) -> if n - ix + 1 > h then "↓ More ↓" else "") <$> current dh <*> scroll
       ghciExecOutput = do
         out <- fmap (fmap T.decodeUtf8) $ foldDyn ($) "" $ leftmost
           [ flip mappend <$> _ghci_execOut ghci
           , flip mappend <$> _ghci_execErr ghci
           , const "" <$ _ghci_filesystem ghci
           ]
-        dh <- displayHeight
-        rec scroll <- scrollableText (tagMaybe (scrollBy <$> current dh <*> scroll) $ updated out) $ current out
-            let scrollBy h (ix, n) =
-                  if | ix == 0 && n <= h -> Nothing -- Scrolled to the top and we don't have to scroll down
-                     | n > h && n - ix - h == 0 -> Just 1
-                     | otherwise -> Nothing
+        let scrollingOutput = do
+              dh <- displayHeight
+              rec scroll <- scrollableText (tagMaybe (scrollBy <$> current dh <*> scroll) $ updated out) $ current out
+                  let scrollBy h (ix, n) =
+                        if | ix == 0 && n <= h -> Nothing -- Scrolled to the top and we don't have to scroll down
+                           | n > h && n - ix - h == 0 -> Just 1
+                           | otherwise -> Nothing
+              return ()
+        -- Rebuild the entire output widget so that we don't have to worry about resetting scroll state
+        _ <- networkHold scrollingOutput $ ffor (_ghci_filesystem ghci) $ \_ -> scrollingOutput
         return ()
   _ <- splitVDrag (hRule doubleBoxStyle) ghciLoadStatus ghciExecOutput
   return $ () <$ exit
