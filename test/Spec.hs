a7a0bae338769f372d4df2dd2b6b56bee5467219
pdlla <chippermonky@gmail.com>
2021-04-05 09:43:44 -0700

    manually specify tests instead of using hspec-discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
 -- hspec auto-discovery stuff
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+-- does not work with current CircleCI image so instead we do it manually for now
+-- {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+
+import Test.Hspec
+
+import qualified Data.Text.ZipperSpec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Data.Text.ZipperSpec"     Data.Text.ZipperSpec.spec
