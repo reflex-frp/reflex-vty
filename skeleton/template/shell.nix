9bb37a2ac148136b4d01e3073bb0cce123153e5e
Ali Abrar <ali.abrar@obsidian.systems>
2026-07-05 20:31:41 -0400

    skeleton: use thunk for style.hs

--- skeleton/template/shell.nix
+++ skeleton/template/shell.nix
@@ -14,13 +14,15 @@ let
       })
       else release.${compiler + "Packages"};
   pkg = hp.callCabal2nix "@PACKAGE_NAME@" (import ./src.nix) { };
+  # fourmolu with the style.hs config auto-applied (see dep/style.hs).
+  fourmolu = import ((import ./dep/style.hs/thunk.nix) + "/fourmolu.nix") { inherit pkgs; };
 in
   pkgs.mkShell {
     name = "shell-${compiler}" + pkgs.lib.optionalString profiling "-profiling";
     buildInputs = [
       pkgs.cabal-install
       pkgs.ghcid
-      pkgs.fourmolu
+      fourmolu
     ];
     inputsFrom = [
       pkg.env
